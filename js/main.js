0070d79397c3bb052a32961cc80005e4435d31a1
Martin Bayer <martin.l.bayer@gmail.com>
2014-02-18 14:48:59 +0100

    added navigation, added routes and states

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-angular.module('portfolio', ['ui.router'])
+angular
+    .module('portfolio', ['ui.router'])
+    .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'templates/home.html'
+            })
+            .state('about', {
+                url: '/about',
+                templateUrl: 'templates/about.html'
+            })
+            .state('contact', {
+                url: '/contact',
+                templateUrl: 'templates/contact.html'
+            })
+    }])
